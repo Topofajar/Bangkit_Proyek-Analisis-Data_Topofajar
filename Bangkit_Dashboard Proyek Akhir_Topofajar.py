bdad84081d14f804abfa4a6b48fe92c827bd7994
Topo Fajar <122133359+Topofajar@users.noreply.github.com>
2024-03-07 08:56:32 +0700

    Update Bangkit_Dashboard Proyek Akhir_Topofajar.py
    hapus library

--- Bangkit_Dashboard Proyek Akhir_Topofajar.py
+++ Bangkit_Dashboard Proyek Akhir_Topofajar.py
@@ -5,14 +5,7 @@
 
 # In[1]:
 
-
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
-import datetime
-sns.set(style='dark')
 
 
 # # Data Frame
